46636ccfebdbea86087476e9b3398facb37c81ef
Vitalii Efimov <g3770888@gmail.com>
2022-08-23 12:48:41 -0400

    started testcase 002

--- src/first/Loop.java
+++ src/first/Loop.java
@@ -2,7 +2,12 @@ package first;
 
 public class Loop {
   public static void main(String[] args){
+
+    int sum = 0;
     for (int i = 0; i < 5; i++) {
+      sum+=i;
+
     }
+    System.out.println(sum);
   }
 }
